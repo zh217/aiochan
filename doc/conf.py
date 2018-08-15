a58643aff4c65dd8c200e2fc51b078e7f255dbd7
Ziyang Hu <hu.ziyang@cantab.net>
2018-08-16 01:21:30 +0800

    refactor docs

--- doc/conf.py
+++ doc/conf.py
@@ -40,6 +40,8 @@ release = '0.1.0'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
+    'nbsphinx',
+    'sphinx.ext.mathjax',
     # 'sphinx.ext.viewcode',
     # 'sphinx.ext.githubpages',
 ]
@@ -66,7 +68,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -83,6 +85,9 @@ html_theme = 'sphinx_rtd_theme'
 # documentation.
 #
 # html_theme_options = {}
+html_theme_options = {
+    "collapse_navigation" : False
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- doc/tutorial.rst
+++ doc/tutorial.rst
@@ -1,17 +1,19 @@
-Tutorial
-========
+A very gentle tutorial to CSP
+=============================
 
-The basics: channels, `select`, and coroutines
-----------------------------------------------
+We will learn how to use CSP with *aiochan*.
 
-Adapted from Rob Pike's *Concurrency Patterns in Go*
+.. toctree::
+   :maxdepth: 2
 
-
-Buffers, graphs, and backpressure
----------------------------------
-
-A tour of other convenient methods and functions
-------------------------------------------------
-
-A step back: understanding the essential API
---------------------------------------------
+   01_concurrency
+   02_csp
+   03_python
+   04_coroutine
+   05_channel
+   06_select
+   07_buffer
+   08_time
+   09_functional
+   10_combine
+   11_parallel
