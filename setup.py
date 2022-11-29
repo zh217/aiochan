d4f93fa051224b7bcdf89930f0c6f7b6ff55b977
Ziyang Hu <hu.ziyang@cantab.net>
2022-11-29 17:43:04 +0800

    Fix #5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aiochan',
-    version='0.2.6',
+    version='0.2.7',
     packages=find_packages(),
     platforms='any',
     classifiers=[
